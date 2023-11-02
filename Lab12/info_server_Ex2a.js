b5a83d912faf8b66b5d4df527456b8ccbaf8ba90
dport96 <dport96@gmail.com>
2023-11-02 11:48:40 -1000

    xxx

---
+++ Lab12/info_server_Ex2a.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.all('*', function (request, response, next) {
+    console.log(request.method + ' to path ' + request.path);
+    next(); 
+});
+
+app.get('/test', function (request, response, next) {
+    response.send('in route GET to /test');
+});
+
+
+
+app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
